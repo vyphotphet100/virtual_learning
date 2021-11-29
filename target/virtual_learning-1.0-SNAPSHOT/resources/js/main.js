a343818903f7738842c77479fe2b7dd8f223258f
Le Duy Tuong <duytuong100101@gmail.com>
2021-11-29 23:42:40 +0700

    edit background class:course

--- target/virtual_learning-1.0-SNAPSHOT/resources/js/main.js
+++ target/virtual_learning-1.0-SNAPSHOT/resources/js/main.js
@@ -69,5 +69,46 @@
     
     /*==================================================================
     [ Add + delete question ]*/
+    var deleteEvent = function (deleteButton) {
+        deleteButton.addEventListener('click', function () {
+            if (document.querySelectorAll('.deleteButton').length < 2) {
+                alert('Must have at least 1 Question');
+            } else {
+                deleteButton.parentNode.parentNode.removeChild(deleteButton.parentNode);
+            }
+        });
+    };
 
+    
+    var deleteButton0 = document.querySelectorAll('.deleteButton');
+    deleteEvent(deleteButton0[0]);
+
+    var addQuestion = document.querySelector('.addQuestion');
+    addQuestion.addEventListener('click', function() {
+        var container = document.querySelector('.question-form-container');
+
+        var div = document.createElement('div');
+        div.setAttribute('class', 'text-center edit-question-form');
+
+        var questionButton = document.createElement('button');
+        questionButton.setAttribute('class', 'join-button join-button-edit font-weight-bold');
+        questionButton.setAttribute('data-toggle', 'modal');
+        questionButton.setAttribute('data-target', '#question');
+        questionButton.setAttribute('style', 'width: 79% !important;');
+        questionButton.innerHTML = 'Question';
+
+        var deleteButton = document.createElement('button');
+        deleteButton.setAttribute('class', 'join-button join-button-edit font-weight-bold deleteButton');
+        deleteButton.setAttribute('data-toggle', 'modal');
+        deleteButton.setAttribute('data-target', '#delete');
+        deleteButton.setAttribute('style', 'width: 20% !important;');
+        deleteButton.innerHTML = 'Delete';
+
+        div.appendChild(questionButton);
+        div.appendChild(deleteButton);
+
+        container.appendChild(div);
+
+        deleteEvent(deleteButton);
+    });
 })(jQuery);
